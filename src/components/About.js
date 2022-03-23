6d6746fea500daa5057128b108eb16d487017dee
Bhavya0207 <bhavyagoel2july@gmail.com>
2022-03-23 15:55:49 +0530

    added rating,date,authors feature with styling

--- src/components/About.js
+++ src/components/About.js
@@ -1,27 +1,38 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import AboutItem from "./AboutItem";
 import LoadingBar from "react-top-loading-bar";
 
 export default function About() {
   let { id } = useParams();
+  let navigate = useNavigate();
+  function redirct() {
+    navigate("/");
+  }
 
   const [book, setBook] = useState();
   const [progress, setProgress] = useState(0);
+  const [error, setError] = useState(false);
 
   const getBookRequest = async () => {
     setProgress(10);
     const url = `https://www.googleapis.com/books/v1/volumes/${id}?key=${process.env.REACT_APP_API_KEY}`;
-    const response = await fetch(url);
-    setProgress(50);
-    
-    const responseJson = await response.json();
-
-    if (responseJson) {
-      setBook(responseJson.volumeInfo);
-      setProgress(100);
+    try {
+      const response = await fetch(url);
+      setProgress(50);
+      if (!response.ok) {
+        setError(true);
+        setProgress(100);
+        return null;
+      }
+      const responseJson = await response.json();
+      if (responseJson) {
+        setBook(responseJson.volumeInfo);
+        setProgress(100);
+      }
+    } catch (error) {
+      return null;
     }
- 
   };
 
   useEffect(() => {
@@ -29,28 +40,48 @@ export default function About() {
     // eslint-disable-next-line
   }, []);
 
+  useEffect(() => {
+    if (error) {
+      setTimeout(() => {
+        redirct();
+      }, 2000);
+    }
+    // eslint-disable-next-line
+  }, [error]);
+
   return (
     <>
-      <LoadingBar
-        color="white"
-        progress={progress}
-        onLoaderFinished={() => setProgress(0)}
-        shadow={true}
-      />
+      {!error && (
+        <LoadingBar
+          color="white"
+          progress={progress}
+          onLoaderFinished={() => setProgress(0)}
+          shadow={true}
+        />
+      )}
 
-      {book && (
+      {error && (
+        <>
+          <h1>No such Book exists</h1>
+          <p>Redirecting back</p>
+        </>
+      )}
+
+      {!error && book && (
         <AboutItem
           title={book.title}
-          authors={book.authors}
+          authors={book.authors ? book.authors : ["unknown"]}
           description={
             book.description ? book.description : "No Description Available"
           }
-          publishedDate={book.publishedDate}
+          publishedDate={book.publishedDate ? book.publishedDate : "unknown"}
           image={
             book.imageLinks
               ? book.imageLinks.thumbnail
               : "https://media.istockphoto.com/vectors/book-flat-icon-vector-id901558740?k=20&m=901558740&s=612x612&w=0&h=LtS2bYY-PwHfx7fyL9sfVX3uQjVym8_z8ZCFJ0euLtY="
           }
+          rating={book.averageRating ? book.averageRating : "0"}
+          raters={book.ratingsCount ? book.ratingsCount : "0"}
         />
       )}
     </>
--- src/components/AboutItem.js
+++ src/components/AboutItem.js
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
-
 function AboutItem(props) {
-  const { title, description, authors, publishedDate, image } = props;
+  const { title, description, authors, publishedDate, image, rating, raters } =
+    props;
+  const [stars, setStars] = useState([]);
+  const getStars = () => {
+    let starArray = [];
+    for (let index = 1; index <= rating; index++) {
+      starArray.push(index);
+    }
+    if (rating % 1 === 0.5) {
+      starArray.push(0.5);
+    }
+    setStars(starArray);
+  };
+  useEffect(() => {
+    getStars();
+    // eslint-disable-next-line
+  }, []);
+
   return (
     <>
       <div className="about-bar">
@@ -12,6 +28,7 @@ function AboutItem(props) {
           Book-Pedia
         </Link>
       </div>
+
       <div className="about-main">
         <img src={image} alt={title} />
         <div className="main-details">
@@ -19,10 +36,40 @@ function AboutItem(props) {
           <p dangerouslySetInnerHTML={{ __html: description }}></p>
         </div>
       </div>
-      {publishedDate}
-      {authors.map((author, index) => {
-        return <p key={index}>{author}</p>;
-      })}
+
+      <div className="details">
+        <div className="authors">
+          <i className="fa-solid fa-pen-to-square"></i>
+          {authors.map((author, index) => {
+            return <p key={index}>{author}</p>;
+          })}
+        </div>
+
+        <div className="date">
+          <i className="fa-regular fa-calendar"></i>
+          <p>{publishedDate}</p>
+        </div>
+      </div>
+
+      <div className="ratings">
+        <div className="rating">
+          {rating}
+          {stars.map((element, index) => {
+            return (
+              <i
+                key={index}
+                className={
+                  element === 0.5
+                    ? "fa-solid fa-star-half-stroke"
+                    : "fa-solid fa-star"
+                }
+              ></i>
+            );
+          })}
+          {" Stars"}
+        </div>
+        <p>*rated by {raters} readers</p>
+      </div>
     </>
   );
 }
